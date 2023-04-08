28197c1e4ef6d33760b1130804ff4ef5adbfe371
David Ewan Campbell <ewan.david@gmail.com>
2023-04-08 16:32:52 +0100

    Card class component added so that I can add route parameters and it can be interacted with by a user in the DOM

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Card extends React.Component {
+  render() {
+    return(
+      <div className='ui raised very padded text container segment'
+        style={{marginTop:'80px'}}>
+
+        <h3 className='ui header'>Card</h3>
+      </div>
+    )
+  }
+}
+
+export default Card;
