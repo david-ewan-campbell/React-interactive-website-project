f98a59f8c1b83e44cdd02849d57f0fcb360f41f9
David Ewan Campbell <ewan.david@gmail.com>
2023-04-08 11:26:02 +0100

    Experimented with a setTimeout to change routes to different url by pushing history object on the props

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Contact = () => {
-
-  return(
-    <div className='ui raised very padded text container segment'
-      style={{marginTop:'80px'}}
-    >
+  return(    
+    <div 
+      className='ui raised very padded text container segment'
+      style={{marginTop:'80px'}}>
+    
       <h3 className='ui header'>Contact</h3>
       <p>Some placeholder text</p>
     </div>
